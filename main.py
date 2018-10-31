8fd49b49e330ff95d075b256c467411645e01b07
Angel Rodriguez <copcos@gmail.com>
2018-10-31 17:09:18 -0400

    Final

--- main.py
+++ main.py
@@ -1,98 +1,70 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template, url_for
 import cgi
-import os
-import jinja2
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+import re
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
-    template = jinja_env.get_template('hello_form.html')
-    return template.render()
-
-@app.route("/hello", methods=['POST'])
-def hello():
-    first_name = request.form['first_name']
-    template = jinja_env.get_template('hello_greeting.html')
-    return template.render(name=first_name)
+app.config['DEBUG'] = True     
 
 
-time_form = """
-    <style>
-        .error {{ color: red; }}
-    </style>
-    <h1>Validate Time</h1>
-    <form method='POST'>
-        <label>Hours (24-hour format)
-            <input name="hours" type="text" value='{hours}' />
-        </label>
-        <p class="error">{hours_error}</p>
-        <label>Minutes
-            <input name="minutes" type="text" value='{minutes}' />
-        </label>
-        <p class="error">{minutes_error}</p>
-        <input type="submit" value="Validate" />
-    </form>
-    """
+@app.route('/welcome', methods=['GET'])
+def welcome():
+    user = request.args.get('username')
+    return render_template('welcome.html', username = user)
 
-@app.route('/validate-time')
-def display_time_form():
-    return time_form.format(hours='', hours_error='',
-        minutes='', minutes_error='')
+@app.route('/', methods=['POST'])
+def validate_password():
+
+    space = [' ']
+
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+
+    if len(username) < 3 or len(username) > 20 or username == "" or any(char in space for char in username):
+        username_error = 'That is not a valid username'
+        username = ''
+    """ if len(password) < 3 or len(password) > 20 or password == "":
+        password_error = 'That is not a valid password'
+        password = '' 
+        username = username
+        email = email """
+    if password == "" or not re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
+        password_error = 'Type 8 char including at least one number, special char, capital and lower letter.'
+        password = '' 
+        username = username
+        email = email     
+    if verify != password or verify == "":
+        verify_error = 'Passwords do not match'
+        username = username
+        password = ''
+        verify = ''
+        email = email 
+          
+    """ if  email != "":
+        if len(email) < 3 or len(email) > 20 or not any(char in special for char in email) """
+    if email != "" and len(email) < 6 or len(email) > 20:
+        if not re.match('\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b', email)!= None:
+            email_error = 'That is not a valid email'
+            username = username
+            password = ''
+            verify = ''
+            email = ''    
 
 
-def is_integer(num):
-    try:
-        int(num)
-        return True
-    except ValueError:
-        return False
-        
-
-@app.route('/validate-time', methods=['POST'])
-def validate_time():
-
-    hours = request.form['hours']
-    minutes = request.form['minutes']
-
-    hours_error = ''
-    minutes_error = ''
-
-    if not is_integer(hours):
-        hours_error = 'Not a valid integer'
-        hours = ''
+    if not username_error and not password_error and not verify_error and not email_error:
+        return redirect( 'welcome?username={}'.format(username) )
     else:
-        hours = int(hours)
-        if hours > 23 or hours < 0:
-            hours_error = 'Hour value out of range (0-23)'
-            hours = ''
-
-    if not is_integer(minutes):
-        minutes_error = 'Not a valid integer'
-        minutes = ''
-    else:
-        minutes = int(minutes)
-        if minutes > 59 or minutes < 0:
-            minutes_error = 'Minutes value out of range (0-59)'
-            minutes = ''
-
-    if not minutes_error and not hours_error:
-        time = str(hours) + ':' + str(minutes)
-        return redirect('/valid-time?time={0}'.format(time))
-    else:
-        return time_form.format(hours_error=hours_error,
-            minutes_error=minutes_error,
-            hours=hours,
-            minutes=minutes)
-
-
-@app.route('/valid-time')
-def valid_time():
-    time = request.args.get('time')
-    return '<h1>You submitted {0}. Thanks for submitting a valid time!</h1>'.format(time)
+        return render_template('register.html',title="Signup", username = username, email = email, username_error = username_error, password_error = password_error, verify_error = verify_error, email_error = email_error)
+  
+@app.route('/')
+def display_index():
+    return render_template('register.html', title = "Signup")
 
 app.run()
